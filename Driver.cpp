6e4d66625e7e718537e40a73c400ae4c31ebb9dd
wcary <wcary@uwyo.edu>
2018-11-24 06:03:45 -0700

    added Test cases
    All test cases passed according to arbitrary standards

--- Driver.cpp
+++ Driver.cpp
@@ -8,6 +8,13 @@ int main() {
 	int pause = 0;
 	Hash test1("lkjfaskdflasjlkdfjlasjdflasjdkfajskldfjoiajwkernvasdkfjasljflkads");
 	Hash test2("hahs");
+	Hash test3("1239dfakdfjj kfjdfkalsjiwero, dfaksd jieriqkjs The lkasjd lkf jCat and Dog when the the Park and shit o nthemselseves");
+	Hash test4("The cat and dog went to the park on 2248 Big Street Boulavard and shit on each other until the cat passed out and the dog was declared the winner");
+	Hash test5("abc");
+	Hash abcTest1("ABC");
+	Hash abcTest2("ABc");
+	Hash char17Test1("The Big Duck is ");
+	Hash char17Test2("the big duck is.");
 	std::cin >> pause;
 	return 2;
 }
