83ad66dc66c2593a377c017b0bffdf23648fdb7e
wcary <wcary@uwyo.edu>
2018-11-24 22:11:22 -0700

    Node.h declaration changes

--- Node.h
+++ Node.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include "Hash.h"
 using namespace std;
 
 #ifndef NODE_H_
@@ -7,17 +8,16 @@ using namespace std;
 
 class Node {
 public:
-	string ID = NULL;
-	string PID = NULL;
-	string EVENT = NULL;
+	Node(string rawEvent);
+	~Node();
+private:
+	string ID;
+	string PID;
+	string EVENT;
 	Hash RHASH();
 	Hash LHASH();
 	vector<Hash> LHIST; //should probably change to a list
 	vector<Hash> RHIST; //should probably change to a list
-	Node();
-	~Node();
-
-private:
 	Node *parent;
 	Node *left;
 	Node *right;
