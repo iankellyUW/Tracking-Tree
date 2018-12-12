19ec463da4f124ad0ead87f89fbd359518b80b11
wcary2 <43153826+wcary2@users.noreply.github.com>
2018-12-12 14:34:07 -0700

    Updated Hash

--- Hash.h
+++ Hash.h
@@ -12,6 +12,8 @@
 #define HASH_SIZE 8
 #define SET_SIZE 62
 
+// Defines all the possible characters the hash 
+// finished hash can possess.
 static constexpr char SET[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b','c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k',
 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
@@ -20,7 +22,7 @@ using namespace std;
 class Hash
 {
 	char getChar(string subset);
-	char hashval[HASH_SIZE] = { '0', '0', '0', '0', '0', '0', '0', '0'};
+	char hashval[HASH_SIZE] = { '0', '0', '0', '0', '0', '0', '0', '0'}; // default hash value
 	const Hash & operator=(const Hash &);
 
 public:
